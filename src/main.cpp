9113634e05094d3f52a688b510afba565acdde19
JustBryse <brysonmacduff@gmail.com>
2024-11-26 20:41:06 -0500

    changed callback function names in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,33 @@
 using namespace Concurrency;
 using namespace Geometry;
 
-void Sleeper()
+void Sleep()
 {
-    spdlog::info("Sleeper() -> Going to sleep.");
+    spdlog::info("Sleep() -> Going to sleep.");
     sleep(1);
-    spdlog::info("Sleeper() -> Waking up.");
+    spdlog::info("Sleep() -> Waking up.");
 }
 
-void TimeoutRaiser()
+void RaiseTimeout()
 {
     const int max_attempts = 5;
     int attempt_cnt = 0;
     while(attempt_cnt < max_attempts)
     {
-        spdlog::info("TimeoutRaiser() -> Pretending to do work " + std::to_string(attempt_cnt+1) + " times.");
+        spdlog::info("RaiseTimeout() -> Pretending to do work " + std::to_string(attempt_cnt+1) + " times.");
         sleep(1);
         ++attempt_cnt;
     }
 
-    spdlog::info("TimeoutRaiser() -> Timeout raised after " + std::to_string(max_attempts) + " attempts.");
+    spdlog::info("RaiseTimeout() -> Timeout raised after " + std::to_string(max_attempts) + " attempts.");
 }
 
 Task AwaitorTask()
 {
-    std::function<void()> sleeper_job = Sleeper;
-    co_await Awaitor(sleeper_job);
-    std::function<void()> timeout_raiser_job = TimeoutRaiser;
-    co_await Awaitor(timeout_raiser_job);
+    std::function<void()> sleep_job = Sleep;
+    co_await Awaitor(sleep_job);
+    std::function<void()> raise_timeout_job = RaiseTimeout;
+    co_await Awaitor(raise_timeout_job);
 }
 
 int main()
